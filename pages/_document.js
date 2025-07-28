9825e1e27c09e39872b2adc4bd1ea3e52cb31daa
ChubbyChuckles <christian.rickert.1989@gmail.com>
2025-07-28 18:43:05 +0200

    added link preview image

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          property="og:title"
+          content="AI Engineering - Innovative Solutions"
+        />
+        <meta
+          property="og:description"
+          content="Explore my cutting-edge AI, and engineering solutions transforming ideas into reality."
+        />
+        <meta
+          property="og:url"
+          content="https://ai-engineering-website.vercel.app/"
+        />
+        <meta
+          property="og:image"
+          content="https://ai-engineering-website.vercel.app/public/og-image.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
