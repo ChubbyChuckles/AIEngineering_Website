661d6cacee7c6eaf50e505890d29a172a11e9945
ChubbyChuckles <christian.rickert.1989@gmail.com>
2025-07-28 23:35:43 +0200

    linked socials

--- components/Socials.js
+++ components/Socials.js
@@ -12,16 +12,36 @@ import {
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link
+        href="https://www.youtube.com/@SSV-Stoetteritz-TT"
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <RiYoutubeLine />
       </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link
+        href="https://www.instagram.com/ssvstoetteritz_tischtennis"
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <RiInstagramLine />
       </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link
+        href="https://www.facebook.com/zuck"
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <RiFacebookBoxLine />
       </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link
+        href="https://github.com/ChubbyChuckles"
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <RiGithubLine />
       </Link>
     </div>
